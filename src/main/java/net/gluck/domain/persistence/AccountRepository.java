4406107ef40b14c19e6de7849b350285afeb009a
JOHN GLUCK <johngluck@JOHNs-MacBook-Pro.local>
2016-03-09 10:27:35 -0800

    Hiding account repository

--- src/main/java/net/gluck/domain/persistence/AccountRepository.java
+++ src/main/java/net/gluck/domain/persistence/AccountRepository.java
@@ -1,9 +1,10 @@
 package net.gluck.domain.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.gluck.domain.Account;
-
+@RepositoryRestResource(exported = false)
 public interface AccountRepository extends CrudRepository<Account, String> {	  
 	  public Account findByUsername(String username);
 }
